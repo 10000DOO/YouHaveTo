efa7c9b0f9d227819bdcff48e94682da40543baf
이건준 <gunjoun99@gmail.com>
2023-01-15 00:08:46 +0900

    fix(loadUserByUsername) optional 처리 수정
    멤버 객체 optional에서 꺼낼 때 null인지 검사하는 로직 추가

--- src/main/java/com/yht/exerciseassist/domain/member/service/MemberService.java
+++ src/main/java/com/yht/exerciseassist/domain/member/service/MemberService.java
@@ -107,8 +107,7 @@ public class MemberService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
         Optional<Member> findMember = memberRepository.findByLoginId(loginId);
-        Member member = findMember.get();
-        return createUserDetails(member);
+        return findMember.map(this::createUserDetails).orElseGet(() -> createUserDetails(null));
     }
 
     // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
