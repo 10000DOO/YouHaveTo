f98bbacde3ee2fc9bd415be0fa1c6947c35bbb99
이건준 <gunjoun99@gmail.com>
2023-02-10 16:41:24 +0900

    chore(docker) : 파일 업로드 경로와 로그파일 생성 경로 설정
    도커 볼륨 사용해서 영속적으로 업로드된 파일과 로그 파일 저장 가능하도록 도커 설정

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM azul/zulu-openjdk:17
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["nohup","java","-jar","/app.jar","&"]
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/yht/exerciseassist/exceoption/controller/DiaryExceptionController.java
+++ src/main/java/com/yht/exerciseassist/exceoption/controller/DiaryExceptionController.java
@@ -6,6 +6,7 @@ import com.yht.exerciseassist.exceoption.dto.ExceptionResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,5 +41,13 @@ public class DiaryExceptionController {
 
         log.error(exception.getMessage());
         return new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ExceptionResponse exceptionHandle(HttpMediaTypeNotSupportedException exception) {
+
+        log.error(exception.getMessage());
+        return new ExceptionResponse(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(), exception.getMessage());
     }
 }
