5d8155fe961b8cf021fc4a4a2f6750edce8956f7
이건준 <gunjoun99@gmail.com>
2023-02-12 14:44:41 +0900

    fix(MemberController) : MemberController 응답 형식 변경
    MemberController 응답 형식을 ResponseEntity를 이용하도록 변경
    기존에 HTTP status 변경 못하던 문제 해결

--- src/main/java/com/yht/exerciseassist/domain/member/controller/MemberController.java
+++ src/main/java/com/yht/exerciseassist/domain/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import com.yht.exerciseassist.domain.member.dto.SignUpRequestDto;
 import com.yht.exerciseassist.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,12 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseResult signUp(@RequestBody @Valid SignUpRequestDto signUpRequestDto) {
+    public ResponseEntity signUp(@RequestBody @Valid SignUpRequestDto signUpRequestDto) {
         return memberService.join(signUpRequestDto);
     }
 
     @PostMapping("/signin")
-    public ResponseResult signIn(@RequestBody @Valid SignInRequestDto signInRequestDto) {
+    public ResponseEntity signIn(@RequestBody @Valid SignInRequestDto signInRequestDto) {
         return memberService.signIn(signInRequestDto.getLoginId(), signInRequestDto.getPassword());
     }
 }
