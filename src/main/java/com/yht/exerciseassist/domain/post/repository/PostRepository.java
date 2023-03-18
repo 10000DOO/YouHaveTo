61e9261d08cd55a23cc80553ebd09ecce027f7f1
이건준 <gunjoun99@gmail.com>
2023-03-18 12:10:07 +0900

    refactor : 게시글 삭제 시 댓글도 삭제 되도록 수정
    게시글 삭제 시 해당 게시글의 댓글도 삭제 되도록 수정

--- src/main/java/com/yht/exerciseassist/domain/post/repository/PostRepository.java
+++ src/main/java/com/yht/exerciseassist/domain/post/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.yht.exerciseassist.domain.post.repository;
 
 import com.yht.exerciseassist.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,5 +11,9 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     @Query(value = "select p from Post p where p.id = :postId and p.dateTime.canceledAt = null")
     Optional<Post> findNotDeletedById(@Param("postId") Long postId);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE Post p SET p.dateTime.canceledAt = :canceledAt WHERE p.id = :postId and p.dateTime.canceledAt = null")
+    void deletePostById(String canceledAt, Long postId);
 }
 
