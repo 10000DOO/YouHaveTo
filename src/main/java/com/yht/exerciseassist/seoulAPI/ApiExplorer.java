a82d16e50215ddde7f1f19301d9fb093d2cbef15
이건준 <gunjoun99@gmail.com>
2023-04-09 21:14:19 +0900

    feat : 페이징 기능 추가
    조회 데이터 페이징 가능하도록 변경

--- src/main/java/com/yht/exerciseassist/seoulAPI/ApiExplorer.java
+++ src/main/java/com/yht/exerciseassist/seoulAPI/ApiExplorer.java
@@ -1,7 +1,12 @@
 package com.yht.exerciseassist.seoulAPI;
 
+import com.yht.exerciseassist.util.ResponseResult;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -18,24 +23,21 @@ public class ApiExplorer {
     @Value("${SeoulAPI.key}")
     private String key;
 
-    public void getSportsFacility(District district, String start, String end) throws IOException {
+    public ResponseResult<JSONObject> getSportsFacility(District district, int offset, int limit) throws IOException, ParseException {
 
         StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088"); /*URL*/
         urlBuilder.append("/" + URLEncoder.encode(key, "UTF-8")); /*인증키 (sample사용시에는 호출시 제한됩니다.)*/
         urlBuilder.append("/" + URLEncoder.encode("json", "UTF-8")); /*요청파일타입 (xml,xmlf,xls,json) */
         urlBuilder.append("/" + URLEncoder.encode(String.valueOf(district), "UTF-8")); /*서비스명 (대소문자 구분 필수입니다.)*/
-        urlBuilder.append("/" + URLEncoder.encode(start, "UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
-        urlBuilder.append("/" + URLEncoder.encode(end, "UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
+        urlBuilder.append("/" + URLEncoder.encode(Integer.toString(offset), "UTF-8")); /*요청시작위치 (sample인증키 사용시 5이내 숫자)*/
+        urlBuilder.append("/" + URLEncoder.encode(Integer.toString(offset + limit - 1), "UTF-8")); /*요청종료위치(sample인증키 사용시 5이상 숫자 선택 안 됨)*/
         // 상위 5개는 필수적으로 순서바꾸지 않고 호출해야 합니다.
-
-        // 서비스별 추가 요청 인자이며 자세한 내용은 각 서비스별 '요청인자'부분에 자세히 나와 있습니다.
-        //urlBuilder.append("/" + URLEncoder.encode("20220301", "UTF-8")); /* 서비스별 추가 요청인자들*/
 
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode()); /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
+        log.info("Response code: " + conn.getResponseCode());/* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
         BufferedReader rd;
 
         // 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
@@ -51,6 +53,29 @@ public class ApiExplorer {
         }
         rd.close();
         conn.disconnect();
-        System.out.println(sb.toString());
+        JSONParser jsonParser = new JSONParser();
+        JSONObject res = (JSONObject) jsonParser.parse(sb.toString());
+        JSONObject districtDetail = (JSONObject) res.get(String.valueOf(district));
+        long listTotalCount = (Long) districtDetail.get("list_total_count");
+
+        if (offset == 1) {
+            districtDetail.put("isFirst", true);
+        } else {
+            districtDetail.put("isFirst", false);
+        }
+
+        if ((offset + limit - 1) > listTotalCount) {
+            districtDetail.put("isLast", true);
+        } else {
+            districtDetail.put("isLast", false);
+        }
+
+        if (listTotalCount / limit == 0) {
+            districtDetail.put("totalPages", listTotalCount / limit);
+        } else {
+            districtDetail.put("totalPages", listTotalCount / limit + 1);
+        }
+
+        return new ResponseResult<>(HttpStatus.OK.value(), res);
     }
 }
--- src/main/java/com/yht/exerciseassist/seoulAPI/controller/SeoulAPIController.java
+++ src/main/java/com/yht/exerciseassist/seoulAPI/controller/SeoulAPIController.java
@@ -2,7 +2,12 @@ package com.yht.exerciseassist.seoulAPI.controller;
 
 import com.yht.exerciseassist.seoulAPI.ApiExplorer;
 import com.yht.exerciseassist.seoulAPI.District;
+import com.yht.exerciseassist.util.ResponseResult;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +21,9 @@ public class SeoulAPIController {
     private final ApiExplorer apiExplorer;
 
     @GetMapping("/api/exercise/seoul")
-    public void getSportsFacility(@RequestParam("district") District district, @RequestParam("start") String start, @RequestParam("end") String end) throws IOException {
+    public ResponseEntity<ResponseResult<JSONObject>> getSportsFacility(@RequestParam("district") District district, @RequestParam("offset") int offset,
+                                                                        @RequestParam("limit") int limit) throws IOException, ParseException {
 
-        apiExplorer.getSportsFacility(district, start, end);
+        return ResponseEntity.status(HttpStatus.OK).body(apiExplorer.getSportsFacility(district, offset, limit));
     }
 }
