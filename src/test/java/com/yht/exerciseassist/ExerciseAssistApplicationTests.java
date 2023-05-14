a84c29f28c5bff74bd5562e6233a74449e167893
이건준 <gunjoun99@gmail.com>
2023-05-14 16:30:40 +0900

    test : 다이어리 테스트 수정
    다이어리 조회에서 DTO 변경으로 인한 테스트 코드 수정

--- src/test/java/com/yht/exerciseassist/ExerciseAssistApplicationTests.java
+++ src/test/java/com/yht/exerciseassist/ExerciseAssistApplicationTests.java
@@ -1,27 +1,8 @@
 package com.yht.exerciseassist;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.yht.exerciseassist.domain.DateTime;
-import com.yht.exerciseassist.domain.member.Member;
-import com.yht.exerciseassist.domain.member.MemberType;
-import com.yht.exerciseassist.domain.member.QMember;
-import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 @SpringBootTest
 class ExerciseAssistApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
