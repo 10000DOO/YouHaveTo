990009dbd7f2a61067eed796707d847eca6c24f5
이건준 <gunjoun99@gmail.com>
2023-07-01 00:34:36 +0900

    fix : 채팅방 생성 시 transaction문제 수정
    @Transaction 붙여 문제 해결

--- src/main/java/com/yht/exerciseassist/domain/chat/repository/ChatListRepository.java
+++ src/main/java/com/yht/exerciseassist/domain/chat/repository/ChatListRepository.java
@@ -1,12 +1,14 @@
 package com.yht.exerciseassist.domain.chat.repository;
 
 import com.yht.exerciseassist.domain.chat.ChatList;
-import com.yht.exerciseassist.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ChatListRepository extends JpaRepository<ChatList, Long> {
 
-    Optional<ChatList> findByMember(Member member);
+    @Query("select c from ChatList c where c.member.id = :memberId")
+    Optional<ChatList> findByMemberId(@Param("memberId") Long memberId);
 }
