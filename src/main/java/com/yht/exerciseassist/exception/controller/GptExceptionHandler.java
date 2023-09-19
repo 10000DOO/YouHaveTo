24a4640c92352520af65d8d1ae446928dd7f7437
10000DOO <gunjoun99@gmail.com>
2023-09-19 16:51:27 +0900

    refactor : 에러 api 통일
    에러 api 통일

--- src/main/java/com/yht/exerciseassist/exception/controller/GptExceptionHandler.java
+++ src/main/java/com/yht/exerciseassist/exception/controller/GptExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.yht.exerciseassist.exception.controller;
 
+import com.yht.exerciseassist.exception.CommonExceptionHandler;
 import com.yht.exerciseassist.exception.dto.ExceptionResponse;
 import com.yht.exerciseassist.exception.error.ErrorCode;
 import com.yht.exerciseassist.gpt.routine.controller.RoutineController;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,13 +16,15 @@ import java.io.IOException;
 
 @Slf4j
 @RestControllerAdvice(assignableTypes = RoutineController.class)
+@RequiredArgsConstructor
 public class GptExceptionHandler {
+
+    private final CommonExceptionHandler commonExceptionHandler;
 
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     @ExceptionHandler(HttpServerErrorException.class)
     public ExceptionResponse httpServerErrorExceptionHandle(IOException exception) {
 
-        log.error("{} // {}", exception.getMessage(), ErrorCode.UNKNOWN_SERVER_ERROR.getMessage());
-        return new ExceptionResponse(HttpStatus.SERVICE_UNAVAILABLE.value(), ErrorCode.UNKNOWN_SERVER_ERROR.getMessage());
+        return commonExceptionHandler.customExceptionRes(exception, log, HttpStatus.SERVICE_UNAVAILABLE.value(), ErrorCode.UNKNOWN_SERVER_ERROR.getMessage());
     }
 }
